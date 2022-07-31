cf4ef89b798d541d4ef3e091f896004a20fb1923
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-07-31 19:38:00 +0600

    update menu appearance

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ with open("./words_dictionary.json", "r", encoding="utf-8") as file:
 
 
 def menu():
+    print()
     print("What would you like to do?")
 
     options = [
@@ -29,6 +30,7 @@ def menu():
 
 def generate_players():
 
+    print()
     print("Who are playing this game?")
     print("Provide comma separated names i.e name, other name, some name.")
 
@@ -51,6 +53,7 @@ def generate_players():
         else:
             usernames += username + ", "
 
+    print()
     print("Are you sure these people will be playing the game?")
     print("Y/N to continue.")
     print(usernames)
@@ -66,12 +69,15 @@ def game():
         current_user = users.current_user
 
         if users.only_one_alive:
+            print()
             print(f"Congratulations! {current_user.name} has won the game.")
+
             break
 
         last_word = words.last_word.word
         last_letter = words.last_word.last_letter
 
+        print()
         print(
             f"`{last_word}` was the previously used word by {words.last_word.user.name}."
         )
@@ -84,6 +90,7 @@ def game():
         if command.is_command:
 
             if command.action == "quit":
+                print()
                 print("Thanks for playing.")
 
                 break
@@ -94,11 +101,13 @@ def game():
 
                     removed_word = words.remove_word()
 
+                    print()
                     print(
-                        f"`{removed_word.word}` entered by {removed_word.user.name} has been removed."
+                        f"`{removed_word.word}` by {removed_word.user.name} has been removed."
                     )
 
                 else:
+                    print()
                     print("Already at the base word.")
 
                 continue
@@ -106,6 +115,7 @@ def game():
             elif command.action == "kill":
                 users.kill_user(current_user)
 
+                print()
                 print(f"{current_user.name} was killed.")
 
                 continue
@@ -113,16 +123,21 @@ def game():
             continue
 
         if words.is_used(user_word_input):
+
+            print()
             print(f"{user_word_input} has already been used.")
 
             continue
 
         if not user_word_input.startswith(last_letter):
+            print()
             print(f"`{user_word_input}` doesn't start with {last_letter}.")
 
             continue
 
         if not user_word_input in dictionary:
+            print()
+
             print(f"`{user_word_input}` is not a valid word.")
 
             continue
@@ -133,6 +148,7 @@ def game():
 
 
 def view_scores():
+    print()
     for user in users.users:
         name = user.name
         score = user.score
